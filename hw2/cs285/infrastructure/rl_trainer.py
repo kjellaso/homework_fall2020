2e7f918562d7b1d3a3f6a7fdfa6b64a83856922e
kjellaso <kjellaso@gmail.com>
2021-02-10 23:15:58 +0100

    Added todos from HW1

--- hw2/cs285/infrastructure/rl_trainer.py
+++ hw2/cs285/infrastructure/rl_trainer.py
@@ -150,16 +150,40 @@ class RL_Trainer(object):
     ####################################
 
     def collect_training_trajectories(self, itr, load_initial_expertdata, collect_policy, batch_size):
-        # TODO: get this from hw1
+        if itr == 0 and load_initial_expertdata is not None:
+            # we are in the first iteration, lets load the expert data
+            with open(load_initial_expertdata, "rb") as fp:
+                loaded_paths = pickle.load(fp)
+                return loaded_paths, 0, None
+
+        print("\nCollecting data to be used for training...")
+        paths, envsteps_this_batch = utils.sample_trajectories(env=self.env, policy=collect_policy, min_timesteps_per_batch=batch_size,
+                                                         max_path_length=self.params['ep_len'])
+
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
+        # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
+        train_video_paths = None
+        if self.log_video:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            train_video_paths = utils.sample_n_trajectories(
+                self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
+
+        return paths, envsteps_this_batch, train_video_paths
+
         # if your load_initial_expertdata is None, then you need to collect new trajectories at *every* iteration
         return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
-        # TODO: get this from hw1
-        return train_logs
+        print('\nTraining agent using sampled data from replay buffer...')
+        all_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(
+                self.params['train_batch_size'])
 
-    ####################################
-    ####################################
+            train_log = self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
+            all_logs.append(train_log)
+        return all_logs
+
 
     def perform_logging(self, itr, paths, eval_policy, train_video_paths, all_logs):
 
--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -86,8 +86,13 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
 
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
-        # TODO: get this from hw1
-        return action
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        # TODO return the action that the policy prescribes -> DONE
+        return self.forward(torch.FloatTensor(obs)).detach().numpy()
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -100,8 +105,10 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor):
         # TODO: get this from hw1
-        return action_distribution
-
+        if self.logits_na != None:
+            return self.logits_na(observation)
+        elif self.mean_net != None:
+            return self.mean_net(observation) 
 
 #####################################################
 #####################################################
